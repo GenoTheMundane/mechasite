1ee4daf60994922f2808a9bc1235fc8af1b91a8b
GenoTheMundane <75117749+GenoTheMundane@users.noreply.github.com>
2024-10-31 15:03:11 +0800

    11ty js update

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,10 @@ module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/_img/");
   eleventyConfig.addPassthroughCopy("src/_art/");
   eleventyConfig.addPassthroughCopy("src/_art/th/");
+  eleventyConfig.addPassthroughCopy("src/_con/");
+  eleventyConfig.addPassthroughCopy("src/_con/th");
   eleventyConfig.addPassthroughCopy("src/_pw/");
   eleventyConfig.addPassthroughCopy("src/_quicksand/");
-  eleventyConfig.addPassthroughCopy("src/_con/");
 
   return {
     dir: {
